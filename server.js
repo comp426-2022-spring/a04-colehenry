488a5c088663588934a120c46f493e21735bfe02
colehenry <crhenry81@gmail.com>
2022-04-29 18:22:58 -0400

    help variable

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const express = require('express')
 const app = express()
 
 const args = require('minimist')(process.argv.slice(2))
+console.log(args)
+
+const help = (`
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
 
 args['port']
 const port = args.port || process.env.PORT || 5000 
@@ -18,33 +36,6 @@ const server = app.listen(port, () => {
 });
 
 
-
-//args["port"]
-
-//const port = args.port || 5000
-
-/*
-if (args.port != null) {
-    port = args.port;
-}
-
-
-// Start an app server
-const server = app.listen(port, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%',port))
-});
-*/
-
-
-
-// Default response for any other request
-
-/*
-app.get('/', (req,res) => {
-    res.send('working test')
-})
-
-*/
 
 app.get('/app/', (req, res) => {
     // Respond with status 200
